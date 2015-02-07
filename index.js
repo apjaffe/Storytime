9e27f132864c95e315d42ec3512850439a68b330
AJ <fortinbox123@googlemail.com>
2015-02-06 23:16:58 -0500

    .gitignore added

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express = require('express')
+var story = requires('./text')
 var app = express();
 
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/story', function(req, res) {
-	  res.send('Hello '+req.query.name+'!')
+	var landmark = req.query.landmark;
+	var cals = req.query.cals;
+	var goto1 = "Entropy";
+	var goto2 = "Walmart";
+	var text = story.onNewLandmark(landmark,cals,goto1,goto2);
+	res.send(text);
 })
 
 app.listen(app.get('port'), function() {
