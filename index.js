c1997c7431d95f90164a97f4e35c4ca167cfac84
AJ <fortinbox123@googlemail.com>
2015-02-07 00:38:30 -0500

    Maps search

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express')
+var request = require('request');
 var story = require('./text')
 var app = express();
+
+var initJSON={"chosenClues":[false,false,false,false],"landmark":"your home","clueCount":0};
+var mapUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyCbFrbx4bfxB51Q8qO7sMbTtVQitGDQM8A&location=$lat%2C$lng&radius=1600&mode=walking";
 
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
@@ -10,15 +14,58 @@ function toBoolean(str)
 	return str==='true';
 }
 
+function cleanLatLng(json)
+{
+	return {"name":json.name,"lat":json.geometry.location.lat,"lng":json.geometry.location.lng};
+}
+
+//callback(goto1,goto2);
+function getLandmarkChoices(lat,lng,callback)
+{
+	var url = mapUrl.replace("$lng",lng).replace("$lat",lat);
+	request.get(url,function(error, response, body)
+	{
+		if(!error && response.statusCode == 200)
+		{
+			var results = JSON.parse(body).results;
+			if(results.length>=2)
+			{
+				var num = Math.floor(Math.random()*(results.length-1));
+				callback(results[num],results[num+1]);
+			}
+			else
+			{
+				callback(null);
+			}
+		}
+	});
+}
 
 app.get('/story', function(req, res) {
-	var a_or_b = toBoolean(req.query.a_or_b);
-	var landmark = req.query.landmark;
-	var cals = req.query.cals;
-	var goto1 = "Entropy";
-	var goto2 = "Walmart";
-	var text = story.onNewLandmark(a_or_b,landmark,cals,goto1,goto2);
-	res.send(text);
+	var json = req.query.json;
+	if(json===undefined || json===null)
+		json = JSON.parse(JSON.stringify(initJSON));
+	getLandmarkChoices(req.query.lat,req.query.lng,function(goto1,goto2)
+	{
+		var text="";
+		if(goto1===null)
+		{
+			text=("You live in a boring place. Sorry.");
+		}
+		else
+		{
+			var a_or_b = toBoolean(req.query.a_or_b);
+			var landmark = json.landmark;
+			var cals = 0;//req.query.cals;
+			text = story.onNewLandmark(json,a_or_b,landmark,cals,goto1.name,goto2.name);
+			json.goto1=cleanLatLng(goto1);
+			json.goto2=cleanLatLng(goto2);
+		}
+		var obj={};
+		obj.json=json;
+		obj.text=text;
+		res.send(JSON.stringify(obj));
+	});
 })
 
 app.listen(app.get('port'), function() {
--- text.js
+++ text.js
@@ -1,26 +1,26 @@
-var arrivedAt = ["You've arrived at ","You're at","Welcome to"];
-var clues = []
-clues[0] = "$";
-clues[1] = "";
-clues[2] = "";
-clues[3] = "";
-clues[4] = "";
-clues[5] = "";
+var arrivedAt = ["You've arrived at ","You're at ","Welcome to "];
+var clues = [];
+clues[0] = "This story begins at a creepy location. Something scary happened. Will you go to $goto1 or $goto2?";
+clues[1] = "Now that you're at $landmark, you notice that you've burned $cals calories. You can go to $goto1 or $goto2.";
+clues[2] = clues[1];
+clues[3] = clues[1];
+clues[4] = clues[1];
+clues[5] = clues[1];
 clues[6] = "";
 clues[7] = "";
 clues[8] = "";
 clues[9] = "";
 
 
-var chosenClues = [false,false,false,false,false];
-clueCount = 0;
 
-exports.onNewLandmark = function (a_or_b, landmark, cals, goto1, goto2) {
+exports.onNewLandmark = function (json, a_or_b, landmark, cals, goto1, goto2) {
+	var clueCount = json.clueCount;
+	var chosenClues = json.chosenClues;
 	chosenClues[clueCount] = a_or_b;
 	var response="";
 	response += arrivedAt[Math.floor(Math.random()*arrivedAt.length)] + landmark;
+	response += ". ";
 	clue = clues[clueCount+chosenClues[clueCount]]; //the relevant clue
-	console.log(clue,clueCount,chosenClues);
 	response += clue.replace("$landmark",landmark).replace("$cals",cals).replace("$goto1",goto1).replace("$goto2",goto2);
 	//response += "Now go to " + goto1 + " to find out what happens to" + x;
 	//response += " or go to " + goto2 + " to find out what happens to" + y;
