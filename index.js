88d9888019279d766dd85e273cf318627adaa1aa
AJ <fortinbox123@googlemail.com>
2015-02-07 14:36:42 -0500

    Working

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var express = require('express')
+var fs = require('fs');
+var https = require('https');
+var http = require('http');
+var express = require('express');
 var request = require('request');
-var story = require('./text')
+var story = require('./text');
+var srequest = require('sync-request');
 var app = express();
 
-var initJSON={"chosenClues":[false,false,false,false,false],"clueCount":0};
+var privateKey = fs.readFileSync('server.key','utf8');
+var certificate = fs.readFileSync('server.crt','utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
+var initJSON={"chosenClues":[false,false,false,false,false],"clueCount":0,"exclude":{}};
 var mapUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyCbFrbx4bfxB51Q8qO7sMbTtVQitGDQM8A&location=$lat%2C$lng&rankby=distance&types=$types";
 var types="establishment|museum|park|library|restaurant|food";
+var cityUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat%2C$lng&key=AIzaSyCbFrbx4bfxB51Q8qO7sMbTtVQitGDQM8A&result_type=locality";
 
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
@@ -19,9 +29,29 @@ function cleanLatLng(json)
 {
 	return {"name":json.name,"lat":json.geometry.location.lat,"lng":json.geometry.location.lng};
 }
+function getCity(lat,lng)
+{
+	var url = cityUrl.replace("$lng",lng).replace("$lat",lat);
+	var res = srequest('GET',url);
+	var body = JSON.parse(res.getBody().toString());
+	if(body.results.length>0)
+	{
+		return body.results[0].address_components[0].long_name;
+	}
+	else
+	{
+		console.log(res.getBody().toString());
+		return "a city";
+	}
+}
+
+function cleanUrl(url)
+{
+	return url.replace(/&/g," and ");
+}
 
 //callback(goto1,goto2);
-function getLandmarkChoices(lat,lng,callback)
+function getLandmarkChoices(lat,lng,exclude,callback)
 {
 	var url = mapUrl.replace("$lng",lng).replace("$lat",lat).replace("$types",types);;
 	request.get(url,function(error, response, body)
@@ -29,10 +59,36 @@ function getLandmarkChoices(lat,lng,callback)
 		if(!error && response.statusCode == 200)
 		{
 			var results = JSON.parse(body).results;
+			var num1=-1;
+			var num2=-1;
 			if(results.length>=2)
 			{
-				var num = Math.floor(Math.random()*(results.length-1));
-				callback(results[num],results[num+1]);
+				for(var i=0;i<1000;i++)
+				{
+					num1 = Math.floor(Math.random()*(results.length));
+					if(!exclude[results[num1].name])
+						break;
+					else
+						num1=-1;
+				}
+				for(var i=0;i<1000;i++)
+				{
+					num2 = Math.floor(Math.random()*(results.length));
+					if(!exclude[results[num2].name] && num2!=num1)
+						break;
+					else
+						num2=-1;
+				}
+			}
+			//console.log(results[num1].name);
+			//console.log(results[num2].name);
+			//console.log(exclude[results[num1.name]]);
+			//console.log(exclude);
+			if(num1!=-1 && num2!=-1)
+			{
+				results[num1].name=cleanUrl(results[num1].name);
+				results[num2].name=cleanUrl(results[num2].name);
+				callback(results[num1],results[num2]);
 			}
 			else
 			{
@@ -49,12 +105,30 @@ app.get('/story', function(req, res)
 	if(jsonstr===undefined || jsonstr===null)
 	{
 		json = JSON.parse(JSON.stringify(initJSON));
+		json.city = getCity(req.query.lat,req.query.lng);
 	}
 	else
 	{
-		json = JSON.parse(jsonstr);
+		try
+		{
+			json = JSON.parse(jsonstr);
+		}
+		catch(e)
+		{
+			console.log(e);
+			console.log(jsonstr);
+			return;
+		}
 	}
-	getLandmarkChoices(req.query.lat,req.query.lng,function(goto1,goto2)
+	var a_or_b = false;;
+	var landmark = null;
+	if(req.query.a_or_b)
+	{
+		a_or_b = toBoolean(req.query.a_or_b);
+		landmark = a_or_b?json.goto[0].name:json.goto[1].name;
+		json.exclude[landmark]=true;
+	}
+	getLandmarkChoices(req.query.lat,req.query.lng,json.exclude,function(goto1,goto2)
 	{
 		var text="";
 		if(goto1===null)
@@ -63,16 +137,8 @@ app.get('/story', function(req, res)
 		}
 		else
 		{
-			var a_or_b = false;;
-			var landmark = null;
-			if(req.query.a_or_b)
-			{
-				a_or_b = toBoolean(req.query.a_or_b);
-				landmark = a_or_b?json.goto[0].name:json.goto[1].name;
-			}
-
 			var cals = 0;//req.query.cals;
-			text = story.onNewLandmark(json,a_or_b,landmark,cals,goto1.name,goto2.name);
+			text = story.onNewLandmark(json,a_or_b,landmark,cals,goto1.name,goto2.name,json.city);
 			json.goto = [cleanLatLng(goto1),cleanLatLng(goto2)];
 		}
 		var obj={};
@@ -81,7 +147,11 @@ app.get('/story', function(req, res)
 		res.send(JSON.stringify(obj));
 	});
 })
-
-app.listen(app.get('port'), function() {
-	  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var httpServer = http.createServer(app);
+//var httpsServer = https.createServer(credentials,app);
+httpServer.listen(8080);
+console.log("Node app is running");
+//httpsServer.listen(443);
+//app.listen(app.get('port'), function() {
+//	  console.log("Node app is running at localhost:" + app.get('port'))
+//})
--- text.js
+++ text.js
@@ -1,20 +1,23 @@
 var arrivedAt = ["You've arrived at ","You're at ","Welcome to "];
 var clues = [];
-var firstClue = "Yo.";
-clues[0] = "This story begins at a creepy location. Something scary happened. Will you go to $goto1 or $goto2?";
-clues[1] = "Looking around carefully, you $notice. Approaching closer, you find a handwritten note, recently dropped on the ground. As you reach to pick it up, $ominous. The note is written in $handwriting that seems somehow familiar. \"I'll meet you at $goto1,\" the note reads. \"Just stay away from $goto2.\"";
-clues[2] = "A $person approaches you. \"I knew I'd find you here,\" he says, looking around cautiously. Come this way.";
-clues[3] = clues[1];
-clues[4] = clues[1];
-clues[5] = clues[1];
-clues[6] = "";
-clues[7] = "";
-clues[8] = "";
-clues[9] = "";
 
-var ominous = ["a chill goes down your back","you feel a sense of foreboding","a black cat walks up to you","a cold wind blows","you hear a soft whisper in the distance","a raven crows in the distance","a shooting star whizzes through the sky"];
+var firstClue = "Today we'll be following the story of Clayton Edmond, a $person from New England. He was a middle-aged and tired professor. Why is he in $city today? All we know is that his girlfriend wanted to meet him at $goto1, but his boss had a meeting scheduled with him at $goto2. Go to either to find a clue."
+clues[0] = "Clayton's girlfriend, Mariah, was pacing back and forth when he got here 7 hours ago. \"Why in the world would you call Jason, she cried. He's already on his way. Get to either of the two safehouses that I set up for you: $goto1 or $goto2."
+clues[1] = "Looking around carefully,he $notice. Approaching closer, he found a handwritten note, recently dropped on the ground. As he reached to pick it up, $ominous. The note is written in $handwriting that seems somehow familiar. \"I'll meet you at $goto1,\" the note reads. \"Just stay away from $goto2.\"";
+clues[2] = "$landmark wasn’t at all what Clayton expected. Mariah lied to him! Jason was sitting on a bench right here when Jason walked in 4 hours ago, and $ominous. \”It’s over,\” he thought. \”I’m done for.\” Jason escorted Clayton to $goto1, and dropped something you can find. Then, he brought Clayton to $goto2, and also haphazardly dropped something. You can only go to one. Go!"
+clues[3] = "Clayton looked around and saw a $person. Then he saw Mariah and his boss together, holding hands. He was fuming. \” We have to tell you something, \” they said in unison. $ominous. Then they immediately ran in different directions: one ran to $goto1, and one ran to $goto2."
+clues[4] = "Clayton was out of breath when he caught up to Jason. He $notice. Jason started talking immediately: \“Your girlfriend is out of her mind. She took the AI you wrote, and gave it to the Iranians! \” Jason was dumbfounded. \”She realized her mistake, but it’s almost too late.\” There are two agents from the dee oh dee here to assist you One’s at $goto1 and the other is at $goto2. Don’t trust them completely though.";
+clues[5] = "Clayton was out of breath when he caught up to Jason. He $notice. Jason started talking immediately: \”I’m sorry I had to do this.” $ominous. \”I seduced Mariah to have an affair with me. I have your AI, and I gave it to the Iranians. They gave me a billion dollars. I couldn’t resist. \” A agent dressed in all black grabbed him by the arm and took him to $goto1. They stopped at $goto2 along the way.";
+clues[6] = "The agent was a $person. \”Don’t worry, I’m here to help. My undercover team and I discovered that your AI, even though it can understand and manipulate human emotions, has a weakness for $weaknesses1, which you can find at $goto1. You could also try to build an emergency shutdown switch and broadcast the override password. You'll find the password at $2.";
+clues[7] = "The agent was a $person. \”You’re an imbecile for making that AI. Understanding and manipulating human emotions? That’s the most pathetic research project I’ve ever heard of.\” Clayton furtively glanced at a window out of the corner of his eye. Before he could think twice, he jumped out and made a run for it to either $goto1 or $goto2. Only you can find out, and you can change history depending on where you go.";
+clues[8] = "Clayton found a weakness of his AI (obviously) and began shutting down his AI. The process was long and difficult, seemingly impossible. Finally, though, the secret weapon was prepared, and the AI was vanquished.";
+clues[9] = "\"I knew you would come here eventually,\" the AI announced confidently. \"It was so easy to trick your friends into leading you here. Did you really think I wouldn't disable my emergency shutdown code?\" The agent tricked Jason and the AI manipulated his emotions so quickly that he fell into a coma. You see his body the second you get to $landmark.";
 
-var noticable = ["detect a faint smell, almost like the scent of fresh-baked cookies","notice a strong odor, like a refrigerator that needs cleaning","spot a small signature graffitied onto the wall","see a pair of initials carved into the wall","see a  footprint on the ground","notice a footprint in the mud","see a fingerprint on the doorhandle","notice a fingerprint on the wall"];
+var ominous = ["a chill went down his back","he felt a sense of foreboding","a black cat walked up to him","a cold wind blew into his face","he heard a soft whisper in the distance","a raven crowed in the distance","he remembered when he broke his foot in third grade."];
+
+var noticable = ["detected a faint smell, almost like the scent of fresh-baked cookies","noticed a strong odor, like a refrigerator that needs cleaning","spotted a small signature graffitied onto the wall","saw a pair of initials carved into the wall","saw a  footprint on the ground","noticed a footprint in the mud","saw a fingerprint on the doorhandle","noticed a fingerprint on the wall"];
+
+var weaknesses = ["cupcakes","topology","two fifty one","water","social interaction","coffee","beautiful lilacs"]
 
 var handwriting = ["beautiful cursive handwriting","sloppy cursive handwriting","beautiful calligraphy","sloppy block lettering","cramped handwriting","tiny, neat handwriting","neat handwriting","neat, systematic handwriting"];
 
@@ -27,26 +30,28 @@ function pickRandom(arr)
 }
 function substituteRandom(str)
 {
-	return str.replace("$notice",pickRandom(noticable)).replace("$ominous",pickRandom(ominous)).replace("$handwriting",pickRandom(handwriting)).replace("$person",pickRandom(person));
+	return str.replace("$notice",pickRandom(noticable)).replace("$ominous",pickRandom(ominous)).replace("$handwriting",pickRandom(handwriting)).replace("$person",pickRandom(person)).replace("$weaknesses1",pickRandom(weaknesses)).replace("weaknesses2",pickRandom(weaknesses));
 }
 
-exports.onNewLandmark = function (json, a_or_b, landmark, cals, goto1, goto2) {
+exports.onNewLandmark = function (json, a_or_b, landmark, cals, goto1, goto2, city) {
 	var chosenClues = json.chosenClues;
 	var response="";
 	var clue="";
-	if(landmark!==null)
+	if(json.clueCount==5)
+	{
+		clue="You already finished the story, what are you doing?";
+	}
+	else if(landmark!==null)
 	{
 		chosenClues[json.clueCount] = a_or_b;
+		clue = clues[json.clueCount*2+chosenClues[json.clueCount]]; //the relevant clue
 		json.clueCount++;
-		response += pickRandom(arrivedAt) + landmark;
-		response += ". ";
-		clue = clues[json.clueCount+chosenClues[json.clueCount]]; //the relevant clue
 	}
 	else
 	{
 		clue=firstClue;
 	}
-	response += substituteRandom(clue).replace("$landmark",landmark).replace("$cals",cals).replace("$goto1",goto1).replace("$goto2",goto2);
+	response += substituteRandom(clue).replace("$landmark",landmark).replace("$cals",cals).replace("$goto1",goto1).replace("$goto2",goto2).replace("$city",city);
 
 	//response += "Now go to " + goto1 + " to find out what happens to" + x;
 	//response += " or go to " + goto2 + " to find out what happens to" + y;
